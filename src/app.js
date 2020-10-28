68254688fac8da5f52cf734fd2d572449df2472b
Adrian Marquis <adrian.marquis78@gmail.com>
2020-10-28 11:05:27 -0400

    routers implementation in app updated

--- src/app.js
+++ src/app.js
@@ -21,16 +21,24 @@ app.use(
     })
 );
 
-app.get('/user', (req, res, next) => {
-       res.send('User info')
-})
-
-app.get('/food', (req, res, next) => {
-    res.send('Food info')
-})
-    
 app.get('/', (req, res) => {
     res.send('Hello, world!')
+})
+
+app.use('/folders', foldersRouter);
+
+app.use('/notes', notesRouter);
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
 })
 
 app.use(function errorHandler(error, req, res, next) {
