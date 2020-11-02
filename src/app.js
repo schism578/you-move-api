859bc46fa18d6b0d175115a02beba55ef8baec69
Adrian Marquis <adrian.marquis78@gmail.com>
2020-11-02 13:45:51 -0500

    cors issue

--- src/app.js
+++ src/app.js
@@ -16,11 +16,6 @@ app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
-
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
