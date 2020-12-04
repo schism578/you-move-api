2d2e86af850444840af9bc28165b0b7075451e4c
Adrian Marquis <adrian.marquis78@gmail.com>
2020-12-04 10:05:35 -0500

    error handling

--- src/app.js
+++ src/app.js
@@ -40,6 +40,7 @@ app.use('/log', calorieRouter)
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
+        console.error(error)
         response = { error: { message: 'server error' } }
     } else {
         console.error(error)
--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://adrianmarquis@localhost/you-move',
   JWT_SECRET: process.env.JWT_SECRET || 'sprechensiedeutsch',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    'http://localhost:8000/api',
+    'http://localhost:8000/',
 }
