bfb7dbc2eaa9dc88fd079381e159616b0abef0dc
Adrian Marquis <adrian.marquis78@gmail.com>
2020-12-16 13:28:39 -0500

    user ID not returned

--- src/user/user-router.js
+++ src/user/user-router.js
@@ -8,7 +8,7 @@ const userRouter = express.Router()
 const jsonParser = express.json()
 
 const serializeUser = user => ({
-  user_id: user.user_id,
+  user_id: user.user_id.value,
   first_name: user.first_name,
   last_name: user.last_name,
   email: user.email,
